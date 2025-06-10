8a1f8649b20aed5830603e93346eacb39f29495e
头脑乱成一团粥 <gkxdavid99@gmail.com>
2025-06-10 17:06:10 +1000

    构建各分栏页面

---
+++ src/pages/Baike.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Sidebar from '../components/Sidebar.vue'
+</script>
+
+<template>
+  <div class="layout-page d-flex">
+    <Sidebar />
+    <main class="flex-fill d-flex justify-content-center align-items-center bg-white text-center">
+      <div>
+      </div>
+    </main>
+  </div>
+</template>
