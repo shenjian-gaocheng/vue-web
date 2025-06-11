511c7c4fb8cf59afcd97ed537e0eafe8011e4666
头脑乱成一团粥 <gkxdavid99@gmail.com>
2025-06-11 18:40:34 +1000

    改成哈希路由，防止刷新404

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Weibo from '../pages/Weibo.vue'
 import Baike from '../pages/Baike.vue'
@@ -18,7 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
