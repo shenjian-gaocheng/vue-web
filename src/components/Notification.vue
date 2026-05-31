da9d87059c67aaf9f12c4b1a99be07281809bd20
gkxKCY <gkxdavid99@gmail.com>
2026-06-01 01:35:03 +1000

    更新顶部通知（总选相关）

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="alert alert-success mb-4 notification-bar" role="note">
-    <h3 class="mb-0">🎂 祝 <strong>周童玥</strong> 3.22 21岁生日快乐！</h3>
+    <h3 class="mb-2">2026 SNH48 GROUP 年度青春盛典 现已开始计分！</h3>
+    <h3 class="mb-0">欢迎各位支持<strong>周童玥</strong>的作品<strong>《For the future》</strong>！<a href="https://ceremony.ckg48.com/list-detail.html?sid=10290">点击查看详情</a></h3>
   </div>
 </template>
 
@@ -14,5 +15,10 @@
   text-align: center;
   font-size: 14px;
   z-index: 9995;
+}
+
+.notification-bar a {
+  color: inherit;
+  font-weight: bold;
 }
 </style>
